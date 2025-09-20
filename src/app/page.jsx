8cde91afdcee0c2960a921916d92286c85371c1d
salman24823 <salman248shah@gmail.com>
2025-09-20 16:24:51 +0500

    setup ready

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="contained" disableElevation>
+        Disable elevation
+      </Button>
+    </div>
+  );
+}
